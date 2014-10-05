dc0fd21f4753c09bd106d4736a91a5163c350ae0
An Tran Minh <atran@atlassian.com>
2014-10-06 00:27:08 +0700

    i18n examples

--- src/test/java/GeneratePdfReportTest.java
+++ src/test/java/GeneratePdfReportTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -37,5 +38,47 @@ public class GeneratePdfReportTest
         final Map<String, Object> params = new HashMap<String, Object>();
         final JasperPrint print = JasperFillManager.fillReport(report, params, new JREmptyDataSource());
         JasperExportManager.exportReportToPdfFile(print, "report2.pdf");
+    }
+
+    @Test
+    public void shouldGeneratePdfReportI18n_US() throws JRException, IOException
+    {
+        final String OUTPUT_FILENAME = "reportI18n_US.pdf";
+        Files.deleteIfExists(Paths.get(OUTPUT_FILENAME));
+
+        final JasperDesign design = JRXmlLoader.load(new File("src/main/resources/I18nReport.jrxml"));
+        final JasperReport report = JasperCompileManager.compileReport(design);
+        final Map<String, Object> params = new HashMap<String, Object>();
+        params.put(JRParameter.REPORT_LOCALE, Locale.US);
+        final JasperPrint print = JasperFillManager.fillReport(report, params, new JREmptyDataSource());
+        JasperExportManager.exportReportToPdfFile(print, OUTPUT_FILENAME);
+    }
+
+    @Test
+    public void shouldGeneratePdfReportI18n_FRENCH() throws JRException, IOException
+    {
+        final String OUTPUT_FILENAME = "reportI18n_FRENCH.pdf";
+        Files.deleteIfExists(Paths.get(OUTPUT_FILENAME));
+
+        final JasperDesign design = JRXmlLoader.load(new File("src/main/resources/I18nReport.jrxml"));
+        final JasperReport report = JasperCompileManager.compileReport(design);
+        final Map<String, Object> params = new HashMap<String, Object>();
+        params.put(JRParameter.REPORT_LOCALE, Locale.FRENCH);
+        final JasperPrint print = JasperFillManager.fillReport(report, params, new JREmptyDataSource());
+        JasperExportManager.exportReportToPdfFile(print, OUTPUT_FILENAME);
+    }
+
+    @Test
+    public void shouldGeneratePdfReportI18n_VIETNAMESE() throws JRException, IOException
+    {
+        final String OUTPUT_FILENAME = "reportI18n_VN.pdf";
+        Files.deleteIfExists(Paths.get(OUTPUT_FILENAME));
+
+        final JasperDesign design = JRXmlLoader.load(new File("src/main/resources/I18nReport.jrxml"));
+        final JasperReport report = JasperCompileManager.compileReport(design);
+        final Map<String, Object> params = new HashMap<String, Object>();
+        params.put(JRParameter.REPORT_LOCALE, new Locale("vi", "vi"));
+        final JasperPrint print = JasperFillManager.fillReport(report, params, new JREmptyDataSource());
+        JasperExportManager.exportReportToPdfFile(print, OUTPUT_FILENAME);
     }
 }
